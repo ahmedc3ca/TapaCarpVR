17060c09df49e78cd65c01db3ec79d7c6c1c14e2
ahmedc3ca <ahmed.ezzo@epfl.ch>
2022-11-01 10:19:23 +0100

    wireframe + button

---
+++ VR/Assets/Scripts/ButtonVR.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonVR : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        isPressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.002f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            sound.Play();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == presser)
+        {
+            button.transform.localPosition = new Vector3(0, 0.014f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void SpawnSphere()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        sphere.transform.localPosition = new Vector3(-1f, 2f, -1f);
+        sphere.AddComponent<Rigidbody>();
+    }
+
+
+}
